02b0d3fde515aa5cb94a642288052da6e8911074
Hohichh <verkovich2005@gmail.com>
2025-10-26 17:39:37 +0300

    add: db entities

--- src/main/java/io/hohichh/marketplace/user/model/CardInfo.java
+++ src/main/java/io/hohichh/marketplace/user/model/CardInfo.java
@@ -1,4 +1,35 @@
 package io.hohichh.marketplace.user.model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "card_info")
 public class CardInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "number", nullable = false, unique = true)
+    private String number;
+
+    @Column(name = "holder", nullable = false)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expiryDate;
 }
--- src/main/java/io/hohichh/marketplace/user/model/User.java
+++ src/main/java/io/hohichh/marketplace/user/model/User.java
@@ -1,4 +1,35 @@
 package io.hohichh.marketplace.user.model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
 }

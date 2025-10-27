7e0bb9cd640e313cd62eab258d3476853c8c63a2
Hohichh <verkovich2005@gmail.com>
2025-10-27 03:39:57 +0300

    add: user service interface

--- src/main/java/io/hohichh/marketplace/user/service/UserService.java
+++ src/main/java/io/hohichh/marketplace/user/service/UserService.java
@@ -1,4 +1,35 @@
 package io.hohichh.marketplace.user.service;
 
+import io.hohichh.marketplace.user.dto.CardInfoDto;
+import io.hohichh.marketplace.user.dto.NewUserDto;
+import io.hohichh.marketplace.user.dto.UserDto;
+import io.hohichh.marketplace.user.dto.UserWithCardsDto;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
+    UserDto createUser(NewUserDto user);
+    void deleteUser(UUID id) throws NotFoundException;
+    UserDto updateUser(UUID id, NewUserDto user) throws NotFoundException;
+    UserWithCardsDto getUserById(UUID id) throws NotFoundException;
+
+    Optional<UserWithCardsDto> getUserByEmail(String email);
+
+    Page<UserDto> getAllUsers(Pageable pageable);
+    List<UserDto> getUsersWithBirthdayToday();
+    Page<UserDto> getUsersBySearchTerm(String searchTerm, Pageable pageable);
+
+    CardInfoDto createCardForUser(CardInfoDto cardInfo) throws NotFoundException;
+    void deleteCard(UUID cardId) throws NotFoundException;
+    CardInfoDto getCardById(UUID cardId) throws NotFoundException;
+
+    Optional<CardInfoDto> getCardByNumber(String cardNumber);
+
+    List<CardInfoDto> getCardsByUserId(UUID userId);
+    List<CardInfoDto> getExpiredCards();
 }

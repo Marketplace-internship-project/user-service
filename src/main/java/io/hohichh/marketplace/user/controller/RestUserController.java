3c873cf096b19682781e77c1e96c99219ddbbd9c
Hohichh <verkovich2005@gmail.com>
2025-10-28 13:08:14 +0300

    add: rest controller end-points

--- src/main/java/io/hohichh/marketplace/user/controller/RestUserController.java
+++ src/main/java/io/hohichh/marketplace/user/controller/RestUserController.java
@@ -1,4 +1,60 @@
 package io.hohichh.marketplace.user.controller;
 
+import io.hohichh.marketplace.user.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/api/v1")
 public class RestUserController {
+    private final UserService userService;
+
+    public RestUserController(UserService userService){
+        this.userService = userService;
+    }
+
+    //user methods======================================================================
+
+    @PostMapping("/users")
+    public void createUser(){}
+
+    @PutMapping("/user/{id}")
+    public void updateUser(){}
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(){}
+
+    @GetMapping("/user/{id}")
+    public void getUserById(){}
+
+    @GetMapping("/user/email={email}")
+    public void getUserByEmail(){}
+
+    @GetMapping("/users")
+    public void getAllUsers(){}
+
+    @GetMapping("/users/birthday=today")
+    public void getUsersWithBirthdayToday(){}
+
+    @GetMapping("/users/search={searchTerm}")
+    public void getUsersBySearchTerm(){}
+
+
+    //card methods======================================================================
+
+    @PostMapping("/user/{user_id}/cards")
+    public void createCardForUser(){}
+
+    @DeleteMapping("/cards/{card_id}")
+    public void deleteCard(){}
+
+    @GetMapping("/cards/{card_id}")
+    public void getCardById(){}
+
+    @GetMapping("/cards/number={card_number}")
+    public void getCardByNumber(){}
+
+    @GetMapping("/user/{user_id}/cards")
+    public void getCardsByUserId(){}
+
+    @GetMapping("/cards/expired")
+    public void getExpiredCards(){}
 }

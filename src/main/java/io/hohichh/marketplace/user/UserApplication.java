a1718ff7c7dd05da57b08cd604059644ea62b45a
Hohichh <verkovich2005@gmail.com>
2025-10-30 21:04:30 +0300

    edit: made java.time.Clock a spring bean

--- src/main/java/io/hohichh/marketplace/user/UserApplication.java
+++ src/main/java/io/hohichh/marketplace/user/UserApplication.java
@@ -3,10 +3,17 @@ package io.hohichh.marketplace.user;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
 
 @SpringBootApplication
 @EnableCaching
 public class UserApplication {
+	@Bean
+	public Clock clock() {
+		return Clock.systemUTC();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}

00c397ab6c85af84963edfb0462b0871b6505c23
Hohichh <verkovich2005@gmail.com>
2025-11-13 00:35:11 +0300

    fix configs

--- src/main/java/io/hohichh/marketplace/user/security/JwtValidator.java
+++ src/main/java/io/hohichh/marketplace/user/security/JwtValidator.java
@@ -4,19 +4,22 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-@Slf4j
+
 @Component
+@Slf4j
 public class JwtValidator {
+
 
     private final SecretKey accessSecret;
 
-    public JwtValidator(@Value("${jwt.secret.access}") String accessSecretString) {
+    public JwtValidator(@Value("${jwt.access.secret}") String accessSecretString) {
         this.accessSecret = Keys.hmacShaKeyFor(accessSecretString.getBytes(StandardCharsets.UTF_8));
     }
 
--- src/main/java/io/hohichh/marketplace/user/service/UserServiceImpl.java
+++ src/main/java/io/hohichh/marketplace/user/service/UserServiceImpl.java
@@ -19,7 +19,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PostAuthorize;
